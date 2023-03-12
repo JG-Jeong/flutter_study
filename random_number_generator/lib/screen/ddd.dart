50f7b04fb4e0d949b475691a57d0c21a9b5946a7
JG-Jeong <jo128g@naver.com>
2023-03-13 00:21:01 +0900

    0313 VideoPlayerPractice commit for today's works.

---
+++ random_number_generator/lib/screen/ddd.dart
@@ -0,0 +1,117 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:random_number_generator/component/number_row.dart';
+import '../constant/color.dart';
+
+class SettingsScreen extends StatefulWidget {
+  final int maxNumber;
+
+  const SettingsScreen({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  double maxNumber = 1000;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    maxNumber = widget.maxNumber.toDouble();
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PRIMARY_COLOR,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              _Body(
+                maxNumber: maxNumber,
+              ),
+              _Footer(
+                maxNumber: maxNumber,
+                onSliderChanged: onSliderChanged,
+                onButtonPressed: onButtonPressed,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    setState(() {
+      maxNumber = val;
+    });
+  }
+
+  void onButtonPressed () {
+    Navigator.of(context).pop(maxNumber.toInt());
+  }
+
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          NumberRow(number: maxNumber.toInt())
+        ],
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final ValueChanged<double> onSliderChanged;
+  final double maxNumber;
+  final VoidCallback onButtonPressed;
+
+  const _Footer(
+      {required this.maxNumber,
+      required this.onSliderChanged,
+      required this.onButtonPressed,
+      Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 1000000,
+          onChanged: onSliderChanged,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: RED_COLOR,
+            ),
+            onPressed: onButtonPressed,
+            child: Text('저장!'),
+          ),
+        )
+      ],
+    );
+  }
+}
--- u_and_i/lib/main.dart
+++ u_and_i/lib/main.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:u_and_i/screen/home_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Homescreen(),
-    )
-  );
+  runApp(MaterialApp(
+    theme: ThemeData(
+      textTheme: TextTheme(
+        headline1: TextStyle(
+          color: Colors.white,
+          fontFamily: 'parisienne',
+          fontSize: 80.0,
+        ),
+      ),
+    ),
+    home: Homescreen(),
+  ));
 }
